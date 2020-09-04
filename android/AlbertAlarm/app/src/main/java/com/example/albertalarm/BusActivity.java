3ca811e3702b307aa90e4ffac12ab8bf4fa90c26
yunju-kang <daria0155@naver.com>
2020-09-04 18:30:53 +0900

    create layout

---
+++ android/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
@@ -0,0 +1,16 @@
+package com.example.albertalarm;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class BusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus);
+    }
+
+}
