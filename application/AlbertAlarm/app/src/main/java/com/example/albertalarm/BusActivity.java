8db901d69dc631a28656b3c6bab68f5c20297fed
yunju-kang <daria0155@naver.com>
2020-09-04 22:41:17 +0900

    define class OnSwipeListener

--- application/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
+++ application/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class BusActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- application/AlbertAlarm/app/src/main/java/com/example/albertalarm/MainActivity.java
+++ application/AlbertAlarm/app/src/main/java/com/example/albertalarm/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.albertalarm;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView example;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        example = (TextView)findViewById(R.id.Example);
+
+        example.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this){
+            @Override
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft(){
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
