17dba2d1f60518d8cd6bc489a1e9fdd88051d1f9
yunju-kang <daria0155@naver.com>
2020-09-07 16:33:27 +0900

    add busActivity layout

--- application/AlbertAlarm/app/src/main/java/com/example/albertalarm/MainActivity.java
+++ application/AlbertAlarm/app/src/main/java/com/example/albertalarm/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.albertalarm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     TextView example;
+    Button btnTraffic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         example = (TextView)findViewById(R.id.Example);
+        btnTraffic = (Button)findViewById(R.id.btnTraffic);
 
         example.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this){
             @Override
             public void onSwipeRight() {
                 Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+
             }
             public void onSwipeLeft(){
                 Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
             }
         });
+
+        btnTraffic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent busIntent = new Intent(view.getContext(), BusActivity.class);
+                startActivityForResult(busIntent, 0);
+            }
+        });
     }
+
 
 }
