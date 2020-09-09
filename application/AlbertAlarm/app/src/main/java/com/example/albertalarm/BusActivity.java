196f425a6204cb423e55d3f0632a1ab25643c44e
yunju-kang <daria0155@naver.com>
2020-09-09 22:56:18 +0900

    drawlayout set

--- application/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
+++ application/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
@@ -1,17 +1,22 @@
 package com.example.albertalarm;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class BusActivity extends AppCompatActivity {
 
+   // View busDrawer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
+
 
 
     }
