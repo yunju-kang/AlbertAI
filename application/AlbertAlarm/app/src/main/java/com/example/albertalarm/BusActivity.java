f00cea75d79b0314a7255f950114bcca566df999
yunju-kang <daria0155@naver.com>
2020-09-14 22:39:33 +0900

    add editText to bus_activity

--- application/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
+++ application/AlbertAlarm/app/src/main/java/com/example/albertalarm/BusActivity.java
@@ -3,20 +3,34 @@ package com.example.albertalarm;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 public class BusActivity extends AppCompatActivity {
 
-   // View busDrawer;
-
+    //Button btnOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
 
+        //btnOpen = (Button)findViewById(R.id.open);
+
+
+        // open DrawerLayout Event
+       /* btnOpen.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                DrawerLayout layDrawer = (DrawerLayout)findViewById(R.id.drawer);
+                if(!layDrawer.isDrawerOpen(Gravity.LEFT)){
+                    layDrawer.openDrawer(Gravity.LEFT);
+                }
+            }
+        });  */
 
 
     }
