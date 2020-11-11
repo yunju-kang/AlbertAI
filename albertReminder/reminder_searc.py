9714ed2a7a4475dfb7c03855fb2ce2534f2273eb
yunju-kang <daria0155@naver.com>
2020-11-11 22:56:08 +0900

    set output

--- albertReminder/reminder_searc.py
+++ albertReminder/reminder_searc.py
@@ -1,63 +1,53 @@
+# -*-coding:utf-8-*-
+
+##라이브러리 호출
+from google_auth_oauthlib.flow import InstalledAppFlow
+from flask import Flask, request, json
+from datetime import date
 import os
 import urllib
 
 '''-------------------------------------------------------------------------------'''
 # 구글 API
 '''-------------------------------------------------------------------------------'''
-## 라이브러리 호출
-from google_auth_oauthlib.flow import InstalledAppFlow
-
-
 ## 구글 클라우드 콘솔에서 다운받은 OAuth 2.0 클라이언트 파일경로 연결
 gcreds_filename = 'C:/Project/albertReminder/gcredentials.json'
-
 
 ## 사용 권한 지정
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-
 ## 새로운 창에서 로그인하여 인증 정보 얻기
 flow = InstalledAppFlow.from_client_secrets_file(gcreds_filename, SCOPES)
 gcreds = flow.run_local_server(port=5500)
-
 
 ##캘린더 일정 가져오기
 from googleapiclient.discovery import build
 
 service = build('calendar', 'v3', credentials=gcreds)
 
-
-
-
-
 '''----------------------------------------------------------------------------------'''
-#발화파라미터 얻기
+# 발화파라미터 얻기
 '''----------------------------------------------------------------------------------'''
-##라이브러리 호출
-from flask import Flask, request, json
-from datetime import date
-
-
-
 ##기능 변수 정의
 app = Flask(__name__)
 
 commonResponse = {
-    'version' : '2.0',
-    'resultCode' : 'OK',
-    'output' : {}
+    'version': '2.0',
+    'resultCode': 'OK',
+    'output': {}
 }
 
-def getUtteranceParameter ():
+
+def getUtteranceParameter():
     data = request.get_json()
     return data['action']['parameters']
-
 
 
 ##연결?
 @app.route('/')
 def index():
     return 'Hello Flask'
+
 
 @app.route('/info', methods=['POST'])
 def info():
@@ -67,9 +57,6 @@ def info():
     response['output']['name'] = 'napier'
     return json.dumps(response)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500, debug=True)
-
 
 '''------------------------------------------------------------------------'''
 # 특정 일정 검색
@@ -78,24 +65,47 @@ if __name__ == '__main__':
 
 @app.route('/reminder/searchSchedule', methods=['POST'])
 def searchSchedule():
+    ## 설정
     utteranceParameter = getUtteranceParameter()
-
-
+    response = commonResponse
+    category = []
+    allDay = []
+    partTime = []
 
     '''---------------------------------------------------------------'''
     # 캘린더 스케쥴 불러오기
     '''---------------------------------------------------------------'''
-    category = []
-    page_token = None
 
+    page_token = None
     while True:
         calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            print(calendar_list_entry['summary'])
 
+    '''-----------------------------------------------------------'''
+    # output 설정
+    '''-----------------------------------------------------------'''
 
-
-    ##기본 변수 설정
-    response = commonResponse
     response['output']['existYn'] = 'N'
 
+    ## 종일 스케줄인 경우
+    if allDay and not partTime:
+        response['output']['existYn'] = 'Y'
+        response['output']['returnDate'] = allDay
+    ## 시간 설정 스케줄인 경우
+    elif not allDay and partTime:
+        response['output']['existYn'] = 'Y'
+        response['output']['returnDate'] = partTime
+    ## 두 가지가 공존할 경우
+    else:
+        response['output']['existYn'] = 'Y'
+        dayTime = allDay + partTime
+        response['output']['returnDate'] = dayTime
 
 
+'''------------------------------------------------------------------'''
+# 포트 연결
+'''------------------------------------------------------------------'''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
