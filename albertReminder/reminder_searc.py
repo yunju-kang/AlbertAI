0d898e669d3623843513b02b0bc4d240d41e8630
yunju-kang <daria0155@naver.com>
2020-11-12 04:02:22 +0900

    getting date -1

--- albertReminder/reminder_searc.py
+++ albertReminder/reminder_searc.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 ##라이브러리 호출
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -6,6 +6,7 @@ from flask import Flask, request, json
 from datetime import date
 import os
 import urllib
+import dateutil
 
 '''-------------------------------------------------------------------------------'''
 # 구글 API
@@ -13,29 +14,22 @@ import urllib
 ## 구글 클라우드 콘솔에서 다운받은 OAuth 2.0 클라이언트 파일경로 연결
 gcreds_filename = 'C:/Project/albertReminder/gcredentials.json'
 
-
 ## 사용 권한 지정
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-
 
 ## 새로운 창에서 로그인하여 인증 정보 얻기
 flow = InstalledAppFlow.from_client_secrets_file(gcreds_filename, SCOPES)
 gcreds = flow.run_local_server(port=5500)
-
 
 ##캘린더 일정 가져오기
 from googleapiclient.discovery import build
 
 service = build('calendar', 'v3', credentials=gcreds)
 
-
-
-
-
 '''----------------------------------------------------------------------------------'''
-#발화파라미터 얻기
+# 발화 파라미터 얻기
 '''----------------------------------------------------------------------------------'''
-##기능 변수 정의
+## 기능 변수 정의
 app = Flask(__name__)
 
 commonResponse = {
@@ -44,10 +38,10 @@ commonResponse = {
     'output': {}
 }
 
-def getUtteranceParameter ():
+
+def getUtteranceParameter():
     data = request.get_json()
     return data['action']['parameters']
-
 
 
 ##연결?
@@ -55,10 +49,11 @@ def getUtteranceParameter ():
 def index():
     return 'Hello Flask'
 
+
 @app.route('/info', methods=['POST'])
 def info():
     data = request.get_json()
-    print(data['test'])
+    # print(data['test'])
     response = commonResponse
     response['output']['name'] = 'napier'
     return json.dumps(response)
@@ -71,8 +66,6 @@ def info():
 
 @app.route('/reminder/searchSchedule', methods=['POST'])
 def searchSchedule():
-
-
     ## 설정
     utteranceParameter = getUtteranceParameter()
     response = commonResponse
@@ -86,7 +79,7 @@ def searchSchedule():
     ## 카테고리 저장
     page_token = None
     while True:
-        calendar_list = service.calendarList().list(pageToken = page_token).execute()
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
         for calendar_list_entry in calendar_list['items']:
             print(calendar_list_entry['summary'])
             category.append(calendar_list_entry['summary'])
@@ -94,34 +87,67 @@ def searchSchedule():
         if not page_token:
             break
 
-    #for i in range(len(calendar_list['items'])):
+    for i in range(len(calendar_list['items'])):
+        calendar_id = calendar_list['items'][i]['id']
+        events_result = service.events().list(calendarId=calendar_id,
+                                              singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+
+        if start:
+            print(start)
+        else:
+            print('no schedule')
+
     '''-----------------------------------------------------------'''
-    #output 설정
+    # output 설정
     '''-----------------------------------------------------------'''
+
     schedule = utteranceParameter['schedule']['value']
 
     response['output']['returnSchedule'] = schedule
     response['output']['existYn'] = 'N'
 
-    ## 종일 스케줄인 경우
-    if allDay and not partTime:
+    if start:
         response['output']['existYn'] = 'Y'
-        response['output']['returnDate'] = allDay
-    ## 시간 설정 스케줄인 경우
-    elif not allDay and partTime:
-        response['output']['existYn'] = 'Y'
-        response['output']['returnDate'] = partTime
-    ## 두 가지가 공존할 경우
+        response['output']['returnDate'] = start
+
     else:
-        response['output']['existYn'] = 'Y'
-        dayTime = allDay + partTime
-        response['output']['returnDate'] = dayTime
+        ['output']['returnDate'] = '일정 없음'
 
     return json.dumps(response)
 
+    '''
+    ## 종일 스케줄인 경우
+    if allDay and not partTime:
+        response['output']['existYn'] = 'Y'
+        response['output']['returnDate'] = start
+
+    ## 시간 설정 스케줄인 경우
+    elif not allDay and partTime:
+        response['output']['existYn'] = 'Y'
+        response['output']['returnDate'] = start
+
+    ## 두 가지가 공존할 경우
+
+    ##스케줄이 없는 경우
+    #else:
+        #response['output']['existYn'] = 'Y'
+        #dayTime = allDay + partTime
+        #response['output']['returnDate'] = dayTime 
+    else:
+        ['output']['returnDate'] = '일정 없음'
+
+    return json.dumps(response)
+
+    '''
+
 
 '''------------------------------------------------------------------'''
-#포트 연결
+# 포트 연결
 '''------------------------------------------------------------------'''
 
 if __name__ == '__main__':
